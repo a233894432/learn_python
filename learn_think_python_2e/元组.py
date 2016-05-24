27a7839e42b0e501b17303621ea8a6983c505c74
diogo <a233894432@gmail.com>
2016-05-24 15:59:56 +0800

    1\ 学习  think python 2e   数据结构的选择

--- learn_think_python_2e/元组.py
+++ learn_think_python_2e/元组.py
@@ -11,3 +11,16 @@ t = [0, 1, 2]
 zip(s, t)
 for pair in zip(s, t):
     print(pair)
+
+# 如果需要遍历一个序列的元素以及其索引号，您可以使用内建函数 enumerate ：
+for index, element in enumerate('abc'):
+    print(index, element)
+
+# 字典和元组
+
+d = {'a': 0, 'b': 1, 'c': 2}
+t = d.items()
+#  print(t) ==>  dict_items([('c', 2), ('a', 0), ('b', 1)])
+from structshape import structshape
+
+print(structshape(d))
